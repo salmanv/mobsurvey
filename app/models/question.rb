3ef07e66fd577f61d8bd888032939340e6b820ee
Salman Vajid <s@vajid.com>
2013-11-13 14:23:29 -0500

    added relationships to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+
+acts_as_list
+has_many :choices
+#acts_as_list plugin in this model enables it to alter the order of appearance for each questino in our survey
 end
