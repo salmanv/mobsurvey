d1aafb416cf8843f9a1f2c50893222e72194a5a7
Salman Vajid <s@vajid.com>
2013-11-13 13:55:25 -0500

    added 3 tables to database: quesiton, choice, and answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Surveyed::Application.routes.draw do
+  resources :answers
+
+  resources :choices
+
+  resources :questions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
